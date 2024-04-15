3b32ca003ca3f53b9f5bc1d37ed9dab53a3d01f8
jshyunn <gur158@naver.com>
2024-04-15 18:49:55 +0900

    예제 추가

--- main.cpp
+++ main.cpp
@@ -1,14 +1,57 @@
+
 #include <QApplication>
 #include <QWidget>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
 
-int main(int argc, char* argv[]) {
 
-    QApplication app(argc, argv);
-    QWidget window;
+class MainWindow : public QWidget {
 
-    window.resize(250, 150);
-    window.setWindowTitle("Packet Analyzer");
-    window.show();
+  public:
+    MainWindow(QWidget *parent = nullptr);
+};
 
-    return app.exec();
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent) {
+
+  auto *grid = new QGridLayout(this);
+  grid->setVerticalSpacing(15);
+  grid->setHorizontalSpacing(10);
+
+  auto *title = new QLabel("Title:", this);
+  grid->addWidget(title, 0, 0, 1, 1);
+  title->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+
+  auto *edt1 = new QLineEdit(this);
+  grid->addWidget(edt1, 0, 1, 1, 1);
+
+  auto *author = new QLabel("Author:", this);
+  grid->addWidget(author, 1, 0, 1, 1);
+  author->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+
+  auto *edt2 = new QLineEdit(this);
+  grid->addWidget(edt2, 1, 1, 1, 1);
+
+  auto *review = new QLabel("Review:", this);
+  grid->addWidget(review, 2, 0, 1, 1);
+  review->setAlignment(Qt::AlignRight | Qt::AlignTop);
+
+  auto *te = new QTextEdit(this);
+  grid->addWidget(te, 2, 1, 3, 1);
+
+  setLayout(grid);
+}
+
+int main(int argc, char *argv[]) {
+
+  QApplication app(argc, argv);
+
+  MainWindow window;
+
+  window.setWindowTitle("Review");
+  window.show();
+
+  return app.exec();
 }
